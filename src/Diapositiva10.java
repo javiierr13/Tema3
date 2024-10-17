0a6b273d465aa04b022cc8282988d56eb3639e51
javiierr13 <fjavierrj6@gmail.com>
2024-10-17 11:13:08 +0200

    actividades diapositivas

---
+++ src/Diapositiva10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Diapositiva10 {
+
+	public static void main(String[] args) {
+
+		Scanner sca = new Scanner(System.in);
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
